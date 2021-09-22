fd079bfddb3b4cf0a37d095856228ea166b5ccd0
ysricky <yosuaricky2606@gmail.com>
2021-09-22 12:24:02 +0700

    add conditional check if array in localstorage is empty

--- src/modules/storageProvider.js
+++ src/modules/storageProvider.js
@@ -3,20 +3,24 @@ import { Todo } from './createNewTodo';
 
 //get local storage from projectsArray and re-convert it into new object
 const storedArray = () => JSON.parse(localStorage.getItem('array'));
-let projectsArray = (storedArray() || []).map((obj) => {
-  const newObjectFromStorage = new Project(obj.name);
-  const projectTodos = obj.todos;
-  projectTodos.forEach((todoFromObj) => {
-    const newTodoFromObj = new Todo(
-      todoFromObj.title,
-      todoFromObj.dueDate,
-      todoFromObj.priority,
-      todoFromObj.description
-    );
-    newObjectFromStorage.addTodo(newTodoFromObj);
+
+if (storedArray()) {
+  let projectsArray = (storedArray() || []).map((obj) => {
+    const newObjectFromStorage = new Project(obj.name);
+    const projectTodos = obj.todos;
+    projectTodos.forEach((todoFromObj) => {
+      const newTodoFromObj = new Todo(
+        todoFromObj.title,
+        todoFromObj.dueDate,
+        todoFromObj.priority,
+        todoFromObj.description
+      );
+      newObjectFromStorage.addTodo(newTodoFromObj);
+    });
+    return newObjectFromStorage;
   });
-  return newObjectFromStorage;
-});
+}
+
 const saveToLocalStorage = () => {
   localStorage.setItem('array', JSON.stringify(projectsArray));
 };
